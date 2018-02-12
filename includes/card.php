f5728c92c2bb5527e7b723ab0b28abf907969b58
chillinhen <c.hillen@gmx.net>
2018-02-12 17:17:42 +0100

    Initial commit

---
+++ includes/card.php
@@ -0,0 +1,8 @@
+<div class="card  mb-4 box-shadow">
+    <img class="card-img-top" src="https://dummyimage.com/600x400/000/00ffd5.png." alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
---
+++ includes/menu.php
@@ -0,0 +1,28 @@
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="#">home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Wohin?</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Vornamen
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Jungen</a>
+            <a class="dropdown-item" href="#">Mädchen</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Wissen</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link">Sprüche</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link">Hebammensuche</a>
+    </li>
+</ul>
